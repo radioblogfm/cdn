dea56ddbf8e96708e460d104df6d2882d1d29847
radioblogfm <61922024+radioblogfm@users.noreply.github.com>
2021-06-14 22:44:31 -0300

    Add files via upload

---
+++ selectsong.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    update_song();
+    setInterval(function() {
+	check_if_modified();
+    }, 30000); //30 seconds
+});
+function check_if_modified()
+{
+    $.ajax({
+    type: 'HEAD',
+    url: 'CurrentSong.txt',
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    dataType: 'text',
+    async: true,
+    ifModified: true,
+    error: function() {
+    },
+    success: function(data, status, xhr) {
+	if(status== 'success')
+	    update_song();
+	}
+    });
+}
+function update_song()
+{
+    $.ajax({
+    type: 'GET',
+    url: 'https://players.rcast.net/status/66600',
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    cache: false,
+    dataType: 'text',
+    async: true,
+    success: function(data, status, xhr) {
+            $('#song_name').text(data);
+        }
+    });
+}
